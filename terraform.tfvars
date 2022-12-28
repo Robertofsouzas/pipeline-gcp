1afba4140caff0a4fe8f8ebd5aaab905c1216538
robertofsouzas <robertofsouzas>
2022-12-28 19:46:41 -0300

    Creating simple bucket

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "datapipeline01-372314"
+region     = "us-central1"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "datapipeline01-372314" {
+  type        = string
+  description = "The Google Cloud Project Id"
+}
+
+variable "region" {
+  type    = string
+  default = "Google Cloud location of resources"
+}
